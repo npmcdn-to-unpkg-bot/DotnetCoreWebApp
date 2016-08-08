8e1b7a40f907bc6a84b26e29d27776f49149e6d7
Ernest Fakudze <ernest.fakudze@scouts.org.uk>
2016-08-08 16:26:27 +0100

    Adds very basic paging and it appears to be working.

--- Controllers/ChinookController.cs
+++ Controllers/ChinookController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using TestProject.Models;
 using System.Linq;
+using System;
 
 namespace WebApplication.Controllers
 {
@@ -14,11 +15,30 @@ namespace WebApplication.Controllers
             _chinookContext = chinookContext;
         }
 
-        public IActionResult Artists()
+        public IActionResult Artists(int? pageNumber, int? pageSize)
         {
             ViewData["Message"] = "Here are some cool artists from Chinook.";
 
-            var artists = _chinookContext.Artist.Take(10).ToList();
+            int pageIndex = pageNumber ?? 1;
+            int sizeOfPage = pageSize ?? 10;
+            if (pageIndex < 1) pageIndex = 1;
+            if (sizeOfPage < 1) sizeOfPage = 5;
+            int skipValue = (sizeOfPage * (pageIndex - 1));
+            int totalRecords = _chinookContext.Artist.Count();
+            int totalNumberOfPages = (int)Math.Ceiling((double)totalRecords / sizeOfPage);
+
+            int offset = (int)((pageNumber - 1) * pageSize + 1);
+            int offsetUpperBound = offset + (pageSize.Value - 1);
+            if (offsetUpperBound > totalRecords) offsetUpperBound = totalRecords;
+
+            ViewBag.totalRecords = totalRecords;
+            ViewBag.totalNumberOfPages = totalNumberOfPages;
+            ViewBag.offset = offset;
+            ViewBag.offsetUpperBound = offsetUpperBound;
+            ViewBag.pageIndex = pageIndex;
+            ViewBag.sizeOfPage = sizeOfPage;
+
+            var artists = _chinookContext.Artist.OrderBy(a => a.Name).Skip(skipValue).Take(sizeOfPage).ToList();
             return View(artists);
 
         }
